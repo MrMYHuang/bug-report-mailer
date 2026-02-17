1fc01a69e483e6e4cf52e3984e893f8040983a2e
Meng-Yuan Huang <myh@live.com>
2026-02-17 20:28:34 +0800

    feat: add OPTIONS method support and CORS configuration for API Gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,13 +69,92 @@ resource "aws_api_gateway_method" "bug_report_mailer_any" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method" "bug_report_mailer_options" {
+  rest_api_id   = aws_api_gateway_rest_api.bug_report_mailer_api.id
+  resource_id   = aws_api_gateway_resource.bug_report_mailer_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "bug_report_mailer_proxy" {
   rest_api_id             = aws_api_gateway_rest_api.bug_report_mailer_api.id
   resource_id             = aws_api_gateway_resource.bug_report_mailer_resource.id
   http_method             = aws_api_gateway_method.bug_report_mailer_any.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.bug_report_mailer.invoke_arn
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.bug_report_mailer.arn}/invocations"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  content_handling        = "CONVERT_TO_TEXT"
+  timeout_milliseconds    = 29000
+}
+
+resource "aws_api_gateway_method_response" "bug_report_mailer_any_200" {
+  rest_api_id = aws_api_gateway_rest_api.bug_report_mailer_api.id
+  resource_id = aws_api_gateway_resource.bug_report_mailer_resource.id
+  http_method = aws_api_gateway_method.bug_report_mailer_any.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = false
+    "method.response.header.Access-Control-Allow-Methods" = false
+    "method.response.header.Access-Control-Allow-Origin"  = false
+  }
+}
+
+resource "aws_api_gateway_integration_response" "bug_report_mailer_any_200" {
+  rest_api_id = aws_api_gateway_rest_api.bug_report_mailer_api.id
+  resource_id = aws_api_gateway_resource.bug_report_mailer_resource.id
+  http_method = aws_api_gateway_method.bug_report_mailer_any.http_method
+  status_code = aws_api_gateway_method_response.bug_report_mailer_any_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+resource "aws_api_gateway_integration" "bug_report_mailer_options" {
+  rest_api_id          = aws_api_gateway_rest_api.bug_report_mailer_api.id
+  resource_id          = aws_api_gateway_resource.bug_report_mailer_resource.id
+  http_method          = aws_api_gateway_method.bug_report_mailer_options.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_MATCH"
+  timeout_milliseconds = 29000
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "bug_report_mailer_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.bug_report_mailer_api.id
+  resource_id = aws_api_gateway_resource.bug_report_mailer_resource.id
+  http_method = aws_api_gateway_method.bug_report_mailer_options.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = false
+    "method.response.header.Access-Control-Allow-Methods" = false
+    "method.response.header.Access-Control-Allow-Origin"  = false
+  }
+}
+
+resource "aws_api_gateway_integration_response" "bug_report_mailer_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.bug_report_mailer_api.id
+  resource_id = aws_api_gateway_resource.bug_report_mailer_resource.id
+  http_method = aws_api_gateway_method.bug_report_mailer_options.http_method
+  status_code = aws_api_gateway_method_response.bug_report_mailer_options_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
 }
 
 resource "aws_lambda_permission" "allow_apigateway_invoke" {
@@ -93,7 +172,13 @@ resource "aws_api_gateway_deployment" "bug_report_mailer_deployment" {
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.bug_report_mailer_resource.id,
       aws_api_gateway_method.bug_report_mailer_any.id,
+      aws_api_gateway_method.bug_report_mailer_options.id,
       aws_api_gateway_integration.bug_report_mailer_proxy.id,
+      aws_api_gateway_integration.bug_report_mailer_options.id,
+      aws_api_gateway_method_response.bug_report_mailer_any_200.id,
+      aws_api_gateway_method_response.bug_report_mailer_options_200.id,
+      aws_api_gateway_integration_response.bug_report_mailer_any_200.id,
+      aws_api_gateway_integration_response.bug_report_mailer_options_200.id,
     ]))
   }
 
@@ -103,6 +188,7 @@ resource "aws_api_gateway_deployment" "bug_report_mailer_deployment" {
 
   depends_on = [
     aws_api_gateway_integration.bug_report_mailer_proxy,
+    aws_api_gateway_integration.bug_report_mailer_options,
   ]
 }
 
